ccb3cb4fec583e10f36d8ec5d6472ae7a99320f5
Sheraff <fpellet@ensc.fr>
2023-11-21 13:16:23 +0100

    encourage fewer renders using react-query replaceEqualDeep

--- src/helpers/useTwitchRequest.tsx
+++ src/helpers/useTwitchRequest.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any -- we should allow `any` as the network response, otherwise we're getting into @tanstack/query territory and it gets complicated fast */
+
 import { Toast, showToast } from "@raycast/api";
 import { useCachedState } from "@raycast/utils";
 import { CACHE_PREFIX, zeroDate } from "./cache";
@@ -5,31 +7,77 @@ import { getHeaders } from "./auth";
 import { useEffect, useState, useRef } from "react";
 import fetch from "node-fetch";
 
-// deep compare objects
-function deepCompare(a: any, b: any): boolean {
-  if (a === b) return true;
-  if (typeof a !== typeof b) return false;
+/**
+ * This function returns `a` if `b` is deeply equal.
+ * If not, it will replace any deeply equal children of `b` with those of `a`.
+ * This can be used for structural sharing between JSON values for example.
+ *
+ * Function taken from {@link https://github.com/TanStack/query/blob/main/packages/query-core/src/utils.ts}
+ */
+function replaceEqualDeep<T>(a: unknown, b: T): T;
+function replaceEqualDeep(a: any, b: any): any {
+  if (a === b) {
+    return a;
+  }
 
-  if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length !== b.length) return false;
-    for (let i = 0; i < a.length; i++) {
-      if (!deepCompare(a[i], b[i])) return false;
+  const array = isPlainArray(a) && isPlainArray(b);
+
+  if (array || (isPlainObject(a) && isPlainObject(b))) {
+    const aSize = array ? a.length : Object.keys(a).length;
+    const bItems = array ? b : Object.keys(b);
+    const bSize = bItems.length;
+    const copy: any = array ? [] : {};
+
+    let equalItems = 0;
+
+    for (let i = 0; i < bSize; i++) {
+      const key = array ? i : bItems[i];
+      copy[key] = replaceEqualDeep(a[key], b[key]);
+      if (copy[key] === a[key]) {
+        equalItems++;
+      }
     }
+
+    return aSize === bSize && equalItems === aSize ? a : copy;
+  }
+
+  return b;
+}
+
+function isPlainArray(value: unknown) {
+  return Array.isArray(value) && value.length === Object.keys(value).length;
+}
+
+/** Copied from {@link https://github.com/jonschlinkert/is-plain-object} */
+function isPlainObject(o: any): o is object {
+  if (!hasObjectPrototype(o)) {
+    return false;
+  }
+
+  // If has no constructor
+  const ctor = o.constructor;
+  if (typeof ctor === "undefined") {
     return true;
   }
 
-  if (typeof a === "object" && a !== null && typeof b === "object" && b !== null) {
-    const keysA = Object.keys(a);
-    const keysB = Object.keys(b);
-    if (keysA.length !== keysB.length) return false;
-    for (const key of keysA) {
-      if (!keysB.includes(key)) return false;
-      if (!deepCompare(a[key], b[key])) return false;
-    }
-    return true;
+  // If has modified prototype
+  const prot = ctor.prototype;
+  if (!hasObjectPrototype(prot)) {
+    return false;
   }
 
-  return false;
+  // If constructor does not have an Object-specific method
+  // eslint-disable-next-line no-prototype-builtins -- we're not modifying it, just checking
+  if (!prot.hasOwnProperty("isPrototypeOf")) {
+    return false;
+  }
+
+  // Most likely a plain Object
+  return true;
+}
+
+function hasObjectPrototype(o: any): boolean {
+  return Object.prototype.toString.call(o) === "[object Object]";
 }
 
 const defaultSelect = (data: any) => data.data;
@@ -69,16 +117,18 @@ export function useTwitchRequest<T>({
         setIsLoading(false);
         if (data && data.data) {
           const next = select(data);
-          if (deepCompare(prevRef.current, next)) return;
-          setPrevious(next);
+          const replaced = replaceEqualDeep(prevRef.current, next);
+          if (replaced === prevRef.current) return;
+          setPrevious(replaced);
           setUpdatedAt(String(Date.now()));
           return;
         }
         if (data.message) {
           showToast({ title: "Error", message: data.message, style: Toast.Style.Failure });
         }
-        if (deepCompare(prevRef.current, initialData)) return;
-        setPrevious(initialData);
+        const replaced = replaceEqualDeep(prevRef.current, initialData);
+        if (replaced === initialData) return;
+        setPrevious(replaced);
         setUpdatedAt(String(Date.now()));
       })
       .catch();
